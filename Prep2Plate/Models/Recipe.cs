61f6e585df98e35ea5575405ba3ad7c85d3c1efc
Zalika Ockleberry <zalikaockleberry@outlook.com>
2017-11-15 18:36:21 -0500

    Edited css and config

---
+++ Prep2Plate/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace Prep2Plate.Models
+{
+    public class Recipe
+    {
+        public string name { get; set; }
+        public List<string> recipeIngredient { get; set; }
+        public int recipeYield { get; set; }
+
+
+
+    }
+}
--- Prep2Plate/Properties/AssemblyInfo.cs
+++ Prep2Plate/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Prep2Plate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("A meal planning app ")]
+[assembly: AssemblyConfiguration("Debug")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Prep2Plate")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
