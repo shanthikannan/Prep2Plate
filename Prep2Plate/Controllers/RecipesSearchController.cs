b4b5ad2e439ca389f4ee40c422e69a0a26653767
shanthikannan.d <shanthikannan.d@gmail.com>
2017-11-21 21:50:08 -0500

    Making yummy web api call and getting the results

--- Prep2Plate/Controllers/RecipesSearchController.cs
+++ Prep2Plate/Controllers/RecipesSearchController.cs
@@ -4,8 +4,13 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Prep2Plate.Context;
 using Prep2Plate.Models;
 
@@ -15,11 +20,58 @@ namespace Prep2Plate.Controllers
     {
         private RecipeContext db = new RecipeContext();
 
+        ////Hosted web API REST Service base url  
+        private string AppId = "bf728886";
+        private string AppKey = "a1608c3e5cc021f4354aa6755f42c294";
+        private string Baseurl = "http://api.yummly.com/v1/";
+        private string requestUrl;
+
+        private List<RecipeSearchResult> GetDataFromYummyApiTask(string SearchString)
+        {
+            requestUrl = "api/recipes?_app_id=" + AppId + "&_app_key=" + AppKey + "&" + SearchString;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                var htttpResponse = client.GetAsync(requestUrl);
+                htttpResponse.Wait();
+                var htttpResult = htttpResponse.Result;
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (!htttpResult.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                //Storing the response details recieved from web api   
+                string strResponse = htttpResult.Content.ReadAsStringAsync().Result;
+                JObject jObect = JObject.Parse(strResponse);
+                IList<JToken> jTokenList = jObect["matches"].Children().ToList();
+                
+                // serialize JSON results into .NET objects
+                IList<RecipeSearchResult> recipeResults = new List<RecipeSearchResult>();
+                foreach(JToken jToken in jTokenList)
+                {
+                    RecipeSearchResult recipeSearchResult = new RecipeSearchResult
+                    {
+                        RecipeName = jToken["recipeName"].ToString(),
+                        ImageUrl = jToken["imageUrlsBySize"]["90"].ToString()
+                    };
+                    recipeResults.Add(recipeSearchResult);
+                }
+                return recipeResults.ToList();
+                //returning the employee list to view  
+            }
+        }
+
         // GET: RecipeSearchResults
         public ActionResult Index()
         {
             return View(db.RecipeSearchResults.ToList());
-        }
+         }
 
         // GET: RecipeSearchResults/Details/5
         public ActionResult Details(string id)
@@ -36,13 +88,24 @@ namespace Prep2Plate.Controllers
             return View(recipeSearchResult);
         }
 
-        public ActionResult OnSearchRecipe()
+        public ActionResult OnSearchRecipe(string recipeSearch)
         {
             //Get the value of the text field
             //Make web api call
             //Update View
-
-            return RedirectToAction("Index");
+            recipeSearch = "chicken";
+            List<RecipeSearchResult> recipeSearchResults = new List<RecipeSearchResult>();
+            recipeSearchResults = GetDataFromYummyApiTask(recipeSearch);
+            if (recipeSearchResults == null)
+            {
+                return RedirectToAction("Create");
+                //Show Error Message
+            }
+            else
+            {
+                var vm = recipeSearchResults[0];
+                return View(vm);
+            }
         }
 
         // GET: RecipeSearchResults/Create
