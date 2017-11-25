008b9acdc3f50dfb51627857f1f0fe295bfdd463
Zalika Ockleberry <zalikaockleberry@outlook.com>
2017-11-25 10:36:29 -0500

    bunch of api tests, pre postman

--- Prep2Plate/Controllers/WebApiController.cs
+++ Prep2Plate/Controllers/WebApiController.cs
@@ -1,16 +1,31 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Prep2Plate.Models;
 
 namespace Prep2Plate.Controllers
 {
     public class WebApiController : Controller
     {
-        // GET: WebApi
-        public ActionResult Index()
+        private string url = "";
+        private static HttpClient yumClient = new HttpClient();
+
+        public YumHelper(string url)
         {
+            this.url = url;
+
+            return  
+        }
+        // GET: WebApi
+
+        public ActionResult ShowRecipe(Recipe recipe)
+        {   
+            title = Console.WriteLine($"Name: {product.Name}\tPrice: {product.Price}\tCategory: {product.Category}");
+
             return View();
         }
     }
