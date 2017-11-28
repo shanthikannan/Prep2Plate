09ee93eb04ff67a540f50a3a78a318c672e4d7e7
Zalika Ockleberry <zalikaockleberry@outlook.com>
2017-11-28 13:03:11 -0500

    postman restSharp code added

--- Prep2Plate/Controllers/WebApiController.cs
+++ Prep2Plate/Controllers/WebApiController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using Prep2Plate.Models;
+ using RestSharp;
 
 namespace Prep2Plate.Controllers
 {
@@ -14,11 +15,15 @@ namespace Prep2Plate.Controllers
         private string url = "";
         private static HttpClient yumClient = new HttpClient();
 
-        public YumHelper(string url)
+        public WebApiController(string url)
         {
-            this.url = url;
+            var client = new RestClient("http://api.yummly.com/v1/api/recipes?_app_id=bf728886&_app_key=a1608c3e5cc021f4354aa6755f42c294&%7B%7Bsearch%7D%7D=&requirePictures=true&allowedDiet%5B%5D=390%5EPescetarian");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "c384ffee-8af8-ab3a-139e-f8310563af3f");
+            request.AddHeader("cache-control", "no-cache");
+            IRestResponse response = client.Execute(request);
 
-            return  
+            return;
         }
         // GET: WebApi
 
---
+++ Prep2Plate/Views/WebApi/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Prep2Plate.Views.WebApi.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Search recipes</h1>
+    
+       
+<input type="text" name="search" placeholder="Search.."/>
+    
+&nbsp;
+</body>
+</html>
