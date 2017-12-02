5e91a860e4ad288632214acd9c57c7f9aeada668
shanthikannan.d <shanthikannan.d@gmail.com>
2017-12-02 01:25:59 -0500

    Remove Recipe From Calendar

--- Prep2Plate/Controllers/RecipeCalendarController.cs
+++ Prep2Plate/Controllers/RecipeCalendarController.cs
@@ -56,10 +56,7 @@ namespace Prep2Plate.Controllers
         // GET: RecipeCalendar/Details/5
         public ActionResult RemoveRecipeFromCalendar(int dayOfWeek, int type)
         {
-            RecipeCalendarData calendarData = new RecipeCalendarData();
-            calendarData.UserName = User.Identity.Name;
-            calendarData.DayOfTheWeek = dayOfWeek;
-            calendarData.TypeOfMeal = type;
+            RecipeCalendarData calendarData =  db.RecipeCalendar.Find(User.Identity.Name, dayOfWeek, type);
             db.RecipeCalendar.Remove(calendarData);
             db.SaveChanges();
             return RedirectToAction("Index", new { id = recipeId });
